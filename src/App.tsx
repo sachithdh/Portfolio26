4d96b07d6023f542907f8d23867bedd7dee6c2bc
sachithdh <sachithdhanushka00@gmail.com>
2026-03-11 00:33:11 +0530

    Comment out unnecessary for this release

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SynthEraHero from "./sections/hero/SynthEraHero";
 import AboutMe from "./sections/aboutMe/AboutMe";
 import SkillsTechnologies from "./sections/skillsTechnologies/SkillsTechnologies";
 import ContactMe from "./sections/contactMe/ContactMe";
-import Blog from "./sections/blog/Blog";
+// import Blog from "./sections/blog/Blog";
 import CreatePost from "./sections/blog/CreatePost";
 import BlogPostPage from "./sections/blog/BlogPostPage";
 import Login from "./sections/auth/Login";
@@ -27,7 +27,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Portfolio />} />
-      <Route path="/blog" element={<Blog />} />
+      {/* <Route path="/blog" element={<Blog />} /> */}
       <Route path="/blog/create" element={<CreatePost />} />
       <Route path="/blog/:id" element={<BlogPostPage />} />
       <Route path="/login" element={<Login />} />
