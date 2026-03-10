ad19bf592372489677e64524309ead15116afdd8
sachithdh <sachithdhanushka00@gmail.com>
2026-03-11 00:35:46 +0530

    Comment out unnecessary for this release

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import AboutMe from "./sections/aboutMe/AboutMe";
 import SkillsTechnologies from "./sections/skillsTechnologies/SkillsTechnologies";
 import ContactMe from "./sections/contactMe/ContactMe";
 // import Blog from "./sections/blog/Blog";
-import CreatePost from "./sections/blog/CreatePost";
-import BlogPostPage from "./sections/blog/BlogPostPage";
-import Login from "./sections/auth/Login";
+// import CreatePost from "./sections/blog/CreatePost";
+// import BlogPostPage from "./sections/blog/BlogPostPage";
+// import Login from "./sections/auth/Login";
 
 function Portfolio() {
   return (
@@ -28,9 +28,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Portfolio />} />
       {/* <Route path="/blog" element={<Blog />} /> */}
-      <Route path="/blog/create" element={<CreatePost />} />
-      <Route path="/blog/:id" element={<BlogPostPage />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/blog/create" element={<CreatePost />} /> */}
+      {/* <Route path="/blog/:id" element={<BlogPostPage />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
     </Routes>
   );
 }
